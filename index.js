7a020b0b4d3dab6a147040ae5dba2a77225b5bac
Mohamed Nossirat <78148222+MohamedNossirat@users.noreply.github.com>
2022-03-24 17:08:53 +0100

    inisialization de projet backend

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {ApolloServer, gql} = require('apollo-server-express');
+
+let world = require("./world");
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = require("./graphql/schemas");
+
+// Provide resolver functions for your schema fields
+const resolvers = require("./graphql/resolver");
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({req}) => ({
+        world: world
+    })
+})
+const app = express();
+app.use(express.static('public'));
+server.start().then(res => {
+    server.applyMiddleware({app});
+    app.listen({port: 4000}, () =>
+        console.log(`🚀 Server ready at
+http://localhost:4000${server.graphqlPath}`)
+    );
+})
---
+++ world.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "name": "La Falafelerie",
+    "logo": "icons/world_logo.png",
+    "money": 0,
+    "score": 0,
+    "totalangels": 0,
+    "activeangels": 0,
+    "angelbonus": 2,
+    "lastupdate": 0,
+    "produits": [
+        {
+            "id": 1,
+            "name": "Hommus",
+            "logo": "icons/menue_icons/hommus.jpg",
+            "cout": 4,
+            "croissance": 1.07,
+            "revenu": 1,
+            "vitesse": 500,
+            "quantite": 1,
+            "timeleft": 0,
+            "managerUnlocked": false,
+        },
+        {
+            "id": 2,
+            "name": "Shawarma",
+            "logo": "icons/menue_icons/shawrma.jpg",
+            "cout": 4,
+            "croissance": 1.07,
+            "revenu": 1,
+            "vitesse": 500,
+            "quantite": 1,
+            "timeleft": 0,
+            "managerUnlocked": false,
+        }
+    ]
+}
